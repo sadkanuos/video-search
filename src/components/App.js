14eee84e82660ff0af3329d78b89da4023c6032b
sadkanuos <sounakdas19960@gmail.com>
2022-10-17 16:02:00 +0530

    Ui components

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import SearchBar from "./SearchBar";
 import youtube from "../apis/youtube";
+import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
 
 class App extends React.Component{
-    state = { videos: [] };
+    state = { videos: [], selectedVideo: null };
     
     onInputSubmit = async (input) => {
         const response = await youtube.get('/search', {
@@ -11,14 +13,20 @@ class App extends React.Component{
                 q: input
             }
         });
-        this.setState({videos: response.data.items});
+        this.setState({ videos: response.data.items });
     };
     
+    onVideoSelect = (video) => {
+        this.setState({ selectedVideo: video });
+    };
+
     render(){
         return (
             <div className="ui container">
                 <SearchBar onFormSubmit={this.onInputSubmit} />
                 Found {this.state.videos.length} videos related to the search.
+                <VideoDetail video={this.state.selectedVideo}/>
+                <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />    
             </div>
         );
     }
